84b0652b0261b07a373947022d5dd4565bd24fe5
ajay-singh123 <127340335+ajay-singh123@users.noreply.github.com>
2023-03-09 00:09:06 +0530

    My first code

---
+++ openca1/src/domen/Demo.java
@@ -0,0 +1,12 @@
+package domen;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("ajay singh");
+		
+		System.out.println("DJ");
+		System.out.println("Jyoti");
+	}
+
+}
